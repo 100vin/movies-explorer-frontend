7eb5fe59fc53b5d14c15d8e583052b0651ccbd20
Stanislav <stolvin@yandex.ru>
2023-02-12 11:25:21 +1000

    #3 fix: Пользователь может отправить запрос с неизмененными данными профиля

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,29 +1,29 @@
 import './Profile.css';
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
-import { useContext, useState } from 'react';
-
+import { useContext, useEffect, useState } from 'react';
+import useFormValidation from '../../hooks/useFormValidation';
 
 const Profile = ({ onUserEdit, onLogout }) => {
   const currentUser = useContext(CurrentUserContext);
-  const [values, setValues] = useState({ 
-    name: currentUser.name, 
-    email: currentUser.email, 
-  });
+  const [isCurrentInfo, setIsCurrentInfo] = useState(false);
+
+  const { values, errors, isValid, handleChange, resetForm } = useFormValidation();
+  const { name = currentUser.name, email = currentUser.email } = values;
+
+  useEffect(() => {
+    setIsCurrentInfo(name === currentUser.name && email === currentUser.email);
+  }, [name, email]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onUserEdit(values);
-  }
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setValues({ ...values, [name]: value }); 
+    isValid && onUserEdit(values);
+    resetForm();
   }
 
   return (
     <main className="main profile">
       <section className="container profile__container">
-        <form className="profile__form" action="/" name="profile" onSubmit={handleSubmit} >
+        <form className="profile__form" name="profile" onSubmit={handleSubmit} >
           <h1 className="profile__title">Привет, {currentUser.name}!</h1>
           <fieldset className="profile__fieldset profile__fieldset_type_inputs">
             <label className="profile__field">
@@ -34,7 +34,7 @@ const Profile = ({ onUserEdit, onLogout }) => {
                 name="name"
                 minLength={2}
                 required
-                value={values.name}
+                value={name || ''}
                 onChange={handleChange}
               />
             </label>
@@ -47,15 +47,18 @@ const Profile = ({ onUserEdit, onLogout }) => {
                 pattern="^([\w]+@([\w-]+\.)+[\w]{2,})?$"
                 // pattern="^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
                 required
-                value={values.email}
+                value={email || ''}
                 onChange={handleChange}
               />
             </label>
           </fieldset>
           <fieldset className="profile__fieldset profile__fieldset_type_buttons">
+            {errors.name && <span className="profile__error">Имя: {errors.name}</span>}
+            {errors.email && <span className="profile__error">E-mail: {errors.email}</span>}
             <button 
-              className="profile__button profile__button_action_edit animation"
+              className={`profile__button profile__button_action_edit ${isValid && !isCurrentInfo ? 'animation' : ''}`}
               type="submit"
+              disabled={!isValid || isCurrentInfo}
             >
               Редактировать
             </button>
